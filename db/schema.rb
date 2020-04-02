73a27d0c6dab2d0eb2939229dae3be84ac545c3d
Mika-Ishikawa <m.i.Slack.102@gmail.com>
2020-04-02 21:11:33 +0900

    users table create

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,16 @@
 ActiveRecord::Schema.define(version: 2020_04_02_084559) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "user_image"
+    t.text "introduction"
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birth_day", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
