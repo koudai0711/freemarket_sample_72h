415fadf815bb5c349ae9d05fa2feb8a50d9e3c19
Mika-Ishikawa <m.i.Slack.102@gmail.com>
2020-04-07 20:48:32 +0900

    define controllers

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -59,4 +59,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def new
+  end
+  
 end
