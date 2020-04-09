a618a24a5b93e710a71864c32dc49b69ca967d8e
koudai0711 <forestman_0711@docomo.ne.jp>
2020-04-09 16:24:46 +0900

    インポートnew 削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_105825) do
+ActiveRecord::Schema.define(version: 2020_04_09_022133) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
