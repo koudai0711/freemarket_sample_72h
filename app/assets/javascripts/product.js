d972b2293bc00eee04a3c08c6ab2065917282535
koudai0711 <forestman_0711@docomo.ne.jp>
2020-04-13 12:49:08 +0900

    マークアップビュー修正

---
+++ app/assets/javascripts/product.js
@@ -0,0 +1,75 @@
+$(document).on('turbolinks:load', ()=> {
+  // 画像用のinputを生成する関数
+  const buildFileField = (num)=> {
+    const html = `<div data-index="${num}" class="js-file_group">
+                    <input class="js-file" type="file"
+                    name="product[images_attributes][${num}][src]"
+                    id="product_images_attributes_${num}_src"><br>
+                    <div class="js-remove">削除</div>
+                  </div>`;
+    return html;
+  }
+   // ラベルのwidth操作
+   function setLabel(images) {
+    //プレビューボックスのwidthを取得し、maxから引くことでラベルのwidthを決定
+    $('.inputArea__input_imageArea_form').css({
+      'width': `calc(620px - (20% * ${images}))`
+      })
+  }
+  // プレビュー用のimgタグを生成する関数
+  const buildImg = (index, url)=> {
+    const html = `<div class="img">
+                    <img class="img_image" data-index="${index}" src="${url}" ">
+                    <div class="img_btn">
+                      <div class="img_btn_edit">編集</div>
+                      <div class="img_btn_destroy">削除</div>
+                  </div>`;
+    
+    return html;
+  }
+
+  // file_fieldのnameに動的なindexをつける為の配列
+  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+  // 既に使われているindexを除外
+  lastIndex = $('.js-file_group:last').data('index');
+  fileIndex.splice(0, lastIndex);
+
+  $('.hidden-destroy').hide();
+
+  $('#image-box').on('change', '.js-file', function(e) {
+   
+    const targetIndex = $(this).parent().data('index');
+    console.log(targetIndex);
+    const changeIndex = targetIndex + 1
+    setLabel(changeIndex);
+    // ファイルのブラウザ上でのURLを取得する
+    const file = e.target.files[0];
+    const blobUrl = window.URL.createObjectURL(file);
+
+    // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
+    if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
+      img.setAttribute('src', blobUrl);
+    } else {  // 新規画像追加の処理
+      $('#previews').append(buildImg(targetIndex, blobUrl));
+      // fileIndexの先頭の数字を使ってinputを作る
+      $('#image-box').append(buildFileField(fileIndex[0]));
+      fileIndex.shift();
+      // 末尾の数に1足した数を追加する
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+    }
+  });
+
+  $('#image-box').on('click', '.js-remove', function() {
+    const targetIndex = $(this).parent().data('index');
+    // 該当indexを振られているチェックボックスを取得する
+    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    // もしチェックボックスが存在すればチェックを入れる
+    if (hiddenCheck) hiddenCheck.prop('checked', true);
+
+    $(this).parent().remove();
+    $(`img[data-index="${targetIndex}"]`).remove();
+
+    // 画像入力欄が0個にならないようにしておく
+    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+  });
+});
