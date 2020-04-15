84d413eb76142cdbad60b79da2497be849361580
koudai0711 <forestman_0711@docomo.ne.jp>
2020-04-15 10:10:13 +0900

    [WIP]出品機能単体テスト

---
+++ spec/factories/product.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+
+  factory :product do
+    name            {"abe"}
+    price           {"100"}
+    description     {"neko"}
+    status          {"neko"}
+    shipping_cost   {"100"}
+    prefecture_id   {"1"}
+    shipping_days   {"1"}
+  end
+end
