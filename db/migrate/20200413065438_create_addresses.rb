1a93ff22670e8a2162e620ef0a819864a1e2f888
Mika-Ishikawa <m.i.Slack.102@gmail.com>
2020-04-17 18:02:21 +0900

    edit now

--- db/migrate/20200413065438_create_addresses.rb
+++ db/migrate/20200413065438_create_addresses.rb
@@ -3,6 +3,16 @@ class CreateAddresses < ActiveRecord::Migration[5.2]
     create_table :addresses do |t|
       t.integer :prefecture_id
       t.timestamps
+
+      :address_last_name
+      :address_first_name
+      :address_last_name_kana
+      :address_first_name_kana
+      :postal_code
+      :prefecture
+      :city
+      :house_number
+      :phone_number
     end
   end
 end
