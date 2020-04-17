c6e811f61a41345d4cb224c78f715c36f002e237
RyoAsai-git <61679701+RyoAsai-git@users.noreply.github.com>
2020-04-17 15:48:09 +0900

    fixed
    コメントアウト削除
    prefixにてルート記入

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,4 @@
 class Address < ApplicationRecord
-  # extend ActiveHash::Associations::ActiveRecordExtensions
-  # belongs_to_active_hash :prefecture
   belongs_to :user
   belongs_to :destination
 end
