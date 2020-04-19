585a31658226b2b84ce2eb719e40513bc9603d4b
Mika-Ishikawa <m.i.Slack.102@gmail.com>
2020-04-19 16:35:54 +0900

    adress table validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,6 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :user
   belongs_to :destination
+
+  validates :address_last_name, :address_first_name, :address_last_name_kana, :address_first_name_kana, :postal_code, :prefecture, :city, :house_number, presence: true
 end
