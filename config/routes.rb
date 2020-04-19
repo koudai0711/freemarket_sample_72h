0017d4d03e0f408d201a442a5d6b17a8a8c55b4d
koudai0711 <forestman_0711@docomo.ne.jp>
2020-04-19 13:17:40 +0900

    [WIP]購入機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,31 @@
 Rails.application.routes.draw do
+  get 'purchase/index'
+  get 'purchase/done'
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
   :sessions => 'users/sessions'
   }
   root 'top#index'
   resources :top
-  resources :products, except: :show
+  resources :cards, only: [:new, :show] do
+    collection do
+      post 'show', to: 'cards#show'
+      post 'pay', to: 'cards#pay'
+      post 'delete', to: 'cards#delete'
+    end
+  end
+
+  resources :products do
+    collection do
+      get 'buy'
+    end
+  end
+  resources :purchase, only: [:index] do
+    collection do
+      get 'index', to: 'purchase#index'
+      post 'pay', to: 'purchase#pay'
+      get 'done', to: 'purchase#done'
+    end
+  end
 
 end
