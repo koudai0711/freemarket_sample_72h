be0389e54e2093bc4d5daa4da6eef8c849b8b4d8
koudai0711 <forestman_0711@docomo.ne.jp>
2020-04-20 18:26:50 +0900

    [WIP]

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,7 @@
 class Image < ApplicationRecord
   mount_uploader  :src,     ImageUploader
   belongs_to      :product, optional: true
+
+  
   validates       :src,     presence: true
 end
--- db/migrate/20200409054432_create_images.rb
+++ db/migrate/20200409054432_create_images.rb
@@ -1,8 +1,8 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-      t.string :src,          null: false
-      t.references :product, foreign_key: true
+      t.string      :src,          null: false
+      t.references  :product,      foreign_key: true
       t.timestamps
     end
   end
