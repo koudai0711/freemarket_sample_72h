8d0c42ce567b53d2fb20a7ae7ac865304cfab24b
maegin516 <takananase0516@i.softbank.jp>
2020-04-21 22:30:06 +0900

    ,
    ,

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_04_13_065438) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "ancestry", null: false
+    t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
