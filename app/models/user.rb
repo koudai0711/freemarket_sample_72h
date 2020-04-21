bb912e2729d2dbd01fda884f0de20d3f3d802a15
Mika-Ishikawa <m.i.Slack.102@gmail.com>
2020-04-21 17:16:15 +0900

    address model unit test

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
 
   validates :nickname, :birth_day, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
-  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
 end
 
