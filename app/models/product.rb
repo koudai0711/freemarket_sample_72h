2c13ce81f0dbd209f82c5b462056ac15ac7e15b2
Mika-Ishikawa <m.i.Slack.102@gmail.com>
2020-04-22 12:41:29 +0900

    image chosei

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
   belongs_to :user 
   belongs_to :category
   has_many :images, dependent: :destroy
-  accepts_nested_attributes_for :images, allow_destroy: true, reject_if: proc { |attributes| attributes['images'].blank? }
+  accepts_nested_attributes_for :images, allow_destroy: true
 
   belongs_to :saler, class_name: "User"
   belongs_to :buyer, class_name: "User"
