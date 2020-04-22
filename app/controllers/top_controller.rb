1b61481a0d815299a4e4aac24b8a0ca6444e117a
koudai0711 <forestman_0711@docomo.ne.jp>
2020-04-22 15:38:07 +0900

    [WIP]トップページ一覧表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,8 @@
 class TopController < ApplicationController
   def index
     @products = Product.all.order('created_at DESC')
-    @images = Image.where(product_id: @products.ids)
+    # @images = Image.where(product_id: @products.ids)
+    # @image = Image.where(product_id: params[:id])
   end
 
 
