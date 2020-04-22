bb170be81c332cce25c387b6a9287a08f7122513
maegin516 <takananase0516@i.softbank.jp>
2020-04-22 10:53:26 +0900

    、
    、

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,8 +64,8 @@ class ProductsController < ApplicationController
     )
     @product_buyer= Product.find(params[:id])
     @product_buyer.update( buyer_id: current_user.id)
-    redirect_to action: 'done'
-  end#完了画面に移動
+    redirect_to action: 'done' #完了画面に移動
+  end
 
   def show
     @image = Image.where(product_id: params[:id])
